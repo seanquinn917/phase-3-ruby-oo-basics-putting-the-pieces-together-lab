54b9bcdb199ba9fe9bf043e6645f87dc2ac3ef84
SeanQuinn <Seanquinn917@gmail.com>
2023-09-11 21:47:17 -0400

    commit tree 1e3d4bec9053e414dcd29d1cd0c3d5ebe6746896

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,32 @@
+class Book
 
+    def initialize(title)
+        @title = title
+    end
+    def title
+        @title 
+    end
+    def author=(name)
+        @name = name
+    end
+    def author
+        @name 
+    end
+    def genre=(genre)
+        @genre = genre
+    end
+    def genre 
+        @genre
+    end
+    def page_count=(count)
+        @count = count
+    end
+    def page_count
+        @count
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+        
+end
 
